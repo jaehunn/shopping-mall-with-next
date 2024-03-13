dfbbd23d6ed090c80af59f2bf3c221cae3c21559
Jaehun <qkdwogns98@gmail.com>
2024-03-13 20:40:32 +0900

    chore: Install SVG loader

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...
+  webpack: (config) => {
+    /** @see https://react-svgr.com/docs/next/ */
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      }
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ src/types/svgr.d.ts
@@ -0,0 +1,13 @@
+declare module "*.svg" {
+  import { FC, SVGProps } from "react";
+
+  const content: FC<SVGProps<SVGElement>>;
+
+  export default content;
+}
+
+declare module "*.svg?url" {
+  const content: string;
+
+  export default content;
+}
