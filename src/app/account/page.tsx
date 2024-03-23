9d9111714b886d671fd621d5a4a78e673a3e194c
Jaehun <qkdwogns98@gmail.com>
2024-03-23 14:53:29 +0900

    auth...

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,9 +1,18 @@
 import { createClient } from "~/lib/supabase/server";
+import { signout } from "./actions";
 
 export default async function Account() {
   const supabase = createClient();
 
   const { data } = await supabase.auth.getUser();
 
-  return <>{data?.user?.email ?? ""}</>;
+  return (
+    <>
+      <form>
+        <input type="text" readOnly value={data?.user?.email ?? ""} />
+
+        <button formAction={signout}>Sign Out</button>
+      </form>
+    </>
+  );
 }
